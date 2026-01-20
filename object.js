4368545dc9885619b77671afa5d3d2709350a2ec
Subhajit Halder <haldersubha625405@gmail.com>
2026-01-20 17:33:01 +0530

    Javascript added

---
+++ object.js
@@ -0,0 +1,18 @@
+// Object  in JS
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+    age: 30,
+    
+}
+console.log(person);
+
+console.log(person.firstName); // Output: John
+console.log(person["lastName"]); // Output: Doe 
+
+
+// Adding a new property
+person.gender = "male";
+console.log(person.gender); // Output:
+console.log(person);
+
